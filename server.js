4d7a907c5da553217b0c756e40a91c4ef6945f7b
emimezaa2 <l23281267@toluca.tecnm.mx>
2026-02-28 22:46:26 -0600

    fix: apuntar package.json a server.js y configurar ruta public

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const port = process.env.PORT || 3000;
 
-// Servir archivos estáticos (HTML, CSS, JS) desde la carpeta 'public'
+// Aquí le decimos que use la carpeta 'public' que ya tienes
 app.use(express.static('public'));
 
+app.get('/', (req, res) => {
+    // Y que busque el index.html dentro de public
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
-
-const PORT = 3000;
-app.listen(PORT, () => {
-    console.log(`Servidor listo en http://localhost:${PORT}`);
+app.listen(port, () => {
+    console.log(`JPN System corriendo en el puerto ${port}`);
 });
